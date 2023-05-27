8b13e519aef711952f920de317a4d2512a3fc6f2
Mitesh-1710 <mitesh.mayavanshi1710@gmail.com>
2023-05-28 00:37:07 +0530

    Added rest template configuration class

--- src/main/java/com/geekster/springboot/assignment/Application.java
+++ src/main/java/com/geekster/springboot/assignment/Application.java
@@ -2,19 +2,12 @@ package com.geekster.springboot.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 
 }
